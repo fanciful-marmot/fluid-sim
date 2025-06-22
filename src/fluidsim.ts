c19b3af8f558255b5fdc5191a30164b13c3bcb09
fanciful-marmot <3170456+fanciful-marmot@users.noreply.github.com>
2025-06-22 13:58:49 -0400

    WIP

--- src/fluidsim.ts
+++ src/fluidsim.ts
@@ -14,9 +14,30 @@ class FluidSim {
 
     // Need ping-pong buffers to track velocity and density
     #bindGroups: Array<{
-        bindGroup: GPUBindGroup;
-        view: GPUTextureView;
-        texture: GPUTexture; // R=density, G=hor_v, B=ver_v
+        bindGroups: {
+            densityAdvect: GPUBindGroup;
+            velocityAdvect: GPUBindGroup;
+            densityDiffusion: GPUBindGroup;
+            output: GPUBindGroup;
+        };
+        data: {
+            density: {
+                texture: GPUTexture;
+                view: GPUTextureView;
+            };
+            velocity: {
+                texture: GPUTexture;
+                view: GPUTextureView;
+            };
+            pressure: {
+                texture: GPUTexture;
+                view: GPUTextureView;
+            };
+            divergence: {
+                texture: GPUTexture;
+                view: GPUTextureView;
+            };
+        };
     }>;
     #pingpong: 0 | 1 = 0;
 
@@ -40,17 +61,45 @@ class FluidSim {
         const [w, h] = this.#size;
 
         // Create initial data
-        const fieldData = new Float32Array(w * h * 4);
+        const densityFieldData = new Float32Array(w * h * 4);
+        for (let x = w / 4; x < (3 * w) / 4; x++) {
+            for (let y = h / 4; y < (3 * h) / 4; y++) {
+                densityFieldData[(x + y * w) * 4] = 1;
+            }
+        }
+        const velocityFieldData = new Float32Array(w * h * 4);
         for (let i = 0; i < w * h * 4; i += 4) {
-            fieldData[i + 0] = Math.random();
-            fieldData[i + 1] = Math.sin(i / h / 16.0);
-            fieldData[i + 2] = 0;
-            fieldData[i + 3] = 0;
+            velocityFieldData[i + 0] = 0;
+            velocityFieldData[i + 1] = 2;
+            velocityFieldData[i + 2] = 1;
+            velocityFieldData[i + 3] = 0;
         }
 
         // Update pipeline
         const bindGroupLayout = this.#device.createBindGroupLayout({
             label: 'Sim Update',
+            entries: [
+                {
+                    binding: 0,
+                    visibility: GPUShaderStage.FRAGMENT,
+                    sampler: {
+                        type: 'filtering',
+                    },
+                },
+                {
+                    binding: 1,
+                    visibility: GPUShaderStage.FRAGMENT,
+                    texture: {},
+                },
+                {
+                    binding: 2,
+                    visibility: GPUShaderStage.FRAGMENT,
+                    texture: {},
+                },
+            ],
+        });
+        const outputBindGroupLayout = this.#device.createBindGroupLayout({
+            label: 'Output',
             entries: [
                 {
                     binding: 0,
@@ -81,19 +130,46 @@ class FluidSim {
             addressModeV: 'repeat',
         });
         this.#bindGroups = new Array(2).fill(0).map((_, i) => {
-            const texture = this.#device.createTexture(fieldDescriptor);
-            const view = texture.createView();
-
+            const densityTexture = this.#device.createTexture(fieldDescriptor);
+            const densityView = densityTexture.createView();
             this.#device.queue.writeTexture(
-                { texture },
-                fieldData,
+                { texture: densityTexture },
+                densityFieldData,
                 { bytesPerRow: w * 4 * 4 },
                 { width: w, height: h }
             );
 
-            return {
-                bindGroup: this.#device.createBindGroup({
-                    label: `Sim Compute ${i}`,
+            const velocityTexture = this.#device.createTexture(fieldDescriptor);
+            const velocityView = velocityTexture.createView();
+            this.#device.queue.writeTexture(
+                { texture: velocityTexture },
+                velocityFieldData,
+                { bytesPerRow: w * 4 * 4 },
+                { width: w, height: h }
+            );
+
+            const pressureTexture = this.#device.createTexture(fieldDescriptor);
+            const pressureView = pressureTexture.createView();
+            // this.#device.queue.writeTexture(
+            //     { texture: pressureTexture },
+            //     pressureFieldData,
+            //     { bytesPerRow: w * 4 * 4 },
+            //     { width: w, height: h }
+            // );
+
+            const divergenceTexture =
+                this.#device.createTexture(fieldDescriptor);
+            const divergenceView = divergenceTexture.createView();
+            // this.#device.queue.writeTexture(
+            //     { texture: divergenceTexture },
+            //     divergenceFieldData,
+            //     { bytesPerRow: w * 4 * 4 },
+            //     { width: w, height: h }
+            // );
+
+            const bindGroups = {
+                densityAdvect: this.#device.createBindGroup({
+                    label: `Sim Compute ${i} - densityAdvect`,
                     layout: bindGroupLayout,
                     entries: [
                         {
@@ -102,12 +178,86 @@ class FluidSim {
                         },
                         {
                             binding: 1,
-                            resource: view,
+                            resource: densityView,
+                        },
+                        {
+                            binding: 2,
+                            resource: velocityView,
                         },
                     ],
                 }),
-                texture,
-                view,
+                velocityAdvect: this.#device.createBindGroup({
+                    label: `Sim Compute ${i} - velocityAdvect`,
+                    layout: bindGroupLayout,
+                    entries: [
+                        {
+                            binding: 0,
+                            resource: sampler,
+                        },
+                        {
+                            binding: 1,
+                            resource: velocityView,
+                        },
+                        {
+                            binding: 2,
+                            resource: velocityView,
+                        },
+                    ],
+                }),
+                densityDiffusion: this.#device.createBindGroup({
+                    label: `Sim Compute ${i} - densityAdvect`,
+                    layout: bindGroupLayout,
+                    entries: [
+                        {
+                            binding: 0,
+                            resource: sampler,
+                        },
+                        {
+                            binding: 1,
+                            resource: densityView,
+                        },
+                        {
+                            binding: 2,
+                            resource: velocityView,
+                        },
+                    ],
+                }),
+                output: this.#device.createBindGroup({
+                    label: `Sim Compute ${i} - Output`,
+                    layout: outputBindGroupLayout,
+                    entries: [
+                        {
+                            binding: 0,
+                            resource: sampler,
+                        },
+                        {
+                            binding: 1,
+                            resource: densityView,
+                        },
+                    ],
+                }),
+            };
+
+            return {
+                bindGroups,
+                data: {
+                    density: {
+                        texture: densityTexture,
+                        view: densityView,
+                    },
+                    velocity: {
+                        texture: velocityTexture,
+                        view: velocityView,
+                    },
+                    pressure: {
+                        texture: pressureTexture,
+                        view: pressureView,
+                    },
+                    divergence: {
+                        texture: divergenceTexture,
+                        view: divergenceView,
+                    },
+                },
             };
         });
 
@@ -138,17 +288,78 @@ class FluidSim {
     }
 
     getOutputBindGroup(): GPUBindGroup {
-        return this.#bindGroups[this.#pingpong].bindGroup;
+        return this.#bindGroups[this.#pingpong].bindGroups.output;
+    }
+
+    #swap(): void {
+        this.#pingpong = this.#pingpong === 0 ? 1 : 0;
     }
 
     encodePass(encoder: GPUCommandEncoder) {
+        // Steps:
+        // advect
+        // diffuse
+        // addForces
+        // computePressure
+        // subtractPressureGradient
+
+        // Encode advection
+        {
+            const pass = encoder.beginRenderPass({
+                label: 'FluidSim density advect',
+                colorAttachments: [
+                    {
+                        view: this.#bindGroups[(this.#pingpong + 1) % 2].data
+                            .density.view,
+                        clearValue: { r: 0, g: 0, b: 0, a: 1 },
+                        loadOp: 'clear',
+                        storeOp: 'store',
+                    },
+                ],
+            });
+            pass.setPipeline(this.#advectPipeline);
+            pass.setViewport(0, 0, this.#size[0], this.#size[1], 0, 1);
+            pass.setScissorRect(0, 0, this.#size[0], this.#size[1]);
+            pass.setBindGroup(
+                0,
+                this.#bindGroups[this.#pingpong].bindGroups.densityAdvect
+            );
+            this.#advectPass.draw(pass);
+            pass.end();
+        }
+        // {
+        //     const pass = encoder.beginRenderPass({
+        //         label: 'FluidSim velocity advect',
+        //         colorAttachments: [
+        //             {
+        //                 view: this.#bindGroups[(this.#pingpong + 1) % 2].data
+        //                     .velocity.view,
+        //                 clearValue: { r: 0, g: 0, b: 0, a: 1 },
+        //                 loadOp: 'clear',
+        //                 storeOp: 'store',
+        //             },
+        //         ],
+        //     });
+        //     pass.setPipeline(this.#advectPipeline);
+        //     pass.setViewport(0, 0, this.#size[0], this.#size[1], 0, 1);
+        //     pass.setScissorRect(0, 0, this.#size[0], this.#size[1]);
+        //     pass.setBindGroup(
+        //         0,
+        //         this.#bindGroups[this.#pingpong].bindGroups.velocityAdvect
+        //     );
+        //     this.#advectPass.draw(pass);
+        //     pass.end();
+        // }
+        this.#swap();
+
         // Encode the diffusion step
         for (let k = 0; k < 20; k++) {
             const pass = encoder.beginRenderPass({
                 label: `FluidSim Diffusion Step ${k}`,
                 colorAttachments: [
                     {
-                        view: this.#bindGroups[(this.#pingpong + 1) % 2].view,
+                        view: this.#bindGroups[(this.#pingpong + 1) % 2].data
+                            .density.view,
                         clearValue: { r: 0, g: 0, b: 0, a: 1 },
                         loadOp: 'clear',
                         storeOp: 'store',
@@ -158,31 +369,15 @@ class FluidSim {
             pass.setPipeline(this.#diffusePipeline);
             pass.setViewport(0, 0, this.#size[0], this.#size[1], 0, 1);
             pass.setScissorRect(0, 0, this.#size[0], this.#size[1]);
-            pass.setBindGroup(0, this.#bindGroups[this.#pingpong].bindGroup);
+            pass.setBindGroup(
+                0,
+                this.#bindGroups[this.#pingpong].bindGroups.densityDiffusion
+            );
             this.#diffusePass.draw(pass);
             pass.end();
-            this.#pingpong = this.#pingpong === 0 ? 1 : 0;
-        }
 
-        // Encode advection
-        const pass = encoder.beginRenderPass({
-            label: 'FluidSim Advect',
-            colorAttachments: [
-                {
-                    view: this.#bindGroups[(this.#pingpong + 1) % 2].view,
-                    clearValue: { r: 0, g: 0, b: 0, a: 1 },
-                    loadOp: 'clear',
-                    storeOp: 'store',
-                },
-            ],
-        });
-        pass.setPipeline(this.#advectPipeline);
-        pass.setViewport(0, 0, this.#size[0], this.#size[1], 0, 1);
-        pass.setScissorRect(0, 0, this.#size[0], this.#size[1]);
-        pass.setBindGroup(0, this.#bindGroups[this.#pingpong].bindGroup);
-        this.#advectPass.draw(pass);
-        pass.end();
-        this.#pingpong = this.#pingpong === 0 ? 1 : 0;
+            this.#swap();
+        }
     }
 }
 
--- src/shaders/fluid-sim.wgsl
+++ src/shaders/fluid-sim.wgsl
@@ -5,14 +5,27 @@ struct VertexOutput {
 
 @group(0) @binding(0) var fieldSampler: sampler;
 @group(0) @binding(1) var fieldTexture: texture_2d<f32>;
+@group(0) @binding(2) var fieldTexture2: texture_2d<f32>;
 
-
-fn getTUV(uv: vec2f) -> vec4f {
-    return textureSample(fieldTexture, fieldSampler, uv);
+fn getTUV(uv: vec2f, field: texture_2d<f32>) -> vec4f {
+    return textureSample(field, fieldSampler, uv);
 }
 
-fn getTXY(xy: vec2f, dimensions: vec2f) -> vec4f {
-    return getTUV((xy + vec2f(0.5)) / dimensions);
+@fragment
+fn fluid_advect(in: VertexOutput) -> @location(0) vec4f {
+    var dimensions = vec2f(textureDimensions(fieldTexture));
+    var pixelStep = vec2(1.0) / dimensions;
+
+    var velocity = getTUV(in.uv, fieldTexture2);
+
+    // Advect
+    var dt = 1.0 / 144.0;
+
+    // Step backwards in time
+    var xy = in.uv - dt * velocity.rg;
+
+    // Interpolate
+    return getTUV(xy, fieldTexture);
 }
 
 @fragment
@@ -20,17 +33,18 @@ fn fluid_diffuse(in: VertexOutput) -> @location(0) vec4f {
     var dimensions = vec2f(textureDimensions(fieldTexture));
     var pixelStep = vec2(1.0) / dimensions;
 
-    var data = getTUV(in.uv);
+    const DIFF = 0.00008;
+
+    var data = getTUV(in.uv, fieldTexture);
 
     // Diffuse
     var dt = 1.0 / 144.0;
-    var diff = 0.0001;
-    var a = dt * diff * dimensions.x * dimensions.y;
+    var a = dt * DIFF * dimensions.x * dimensions.y;
     var density = data.r + a * (
-        getTUV(in.uv + pixelStep * vec2(-1.0, 0.0)).r +
-        getTUV(in.uv + pixelStep * vec2(1.0, 0.0)).r +
-        getTUV(in.uv + pixelStep * vec2(0.0, 1.0)).r +
-        getTUV(in.uv + pixelStep * vec2(0.0, -1.0)).r -
+        getTUV(in.uv + pixelStep * vec2(-1.0, 0.0), fieldTexture).r +
+        getTUV(in.uv + pixelStep * vec2(1.0, 0.0), fieldTexture).r +
+        getTUV(in.uv + pixelStep * vec2(0.0, 1.0), fieldTexture).r +
+        getTUV(in.uv + pixelStep * vec2(0.0, -1.0), fieldTexture).r -
         4.0 * data.r
     );
 
@@ -39,39 +53,64 @@ fn fluid_diffuse(in: VertexOutput) -> @location(0) vec4f {
     return color_out;
 }
 
-@fragment
-fn fluid_advect(in: VertexOutput) -> @location(0) vec4f {
-    var dimensions = vec2f(textureDimensions(fieldTexture));
-    var pixelStep = vec2(1.0) / dimensions;
+// @fragment
+// fn fluid_divergence(in: VertexOutput) -> @location(0) vec4f {
+//     var dimensions = vec2f(textureDimensions(fieldTexture));
+//     var pixelStep = vec2(1.0) / dimensions;
 
-    var data = getTUV(in.uv);
+//     var data = getTUV(in.uv);
 
-    // Advect
-    var dt = 1.0 / 144.0;
-    var dt0 = dt * dimensions.x;
-    var ij: vec2f = floor(in.uv / pixelStep);
+//     var divergence = 0.5 * pixelStep.x * (
+//         getTUV(in.uv + pixelStep * vec2(1.0, 0.0)).g -
+//         getTUV(in.uv + pixelStep * vec2(-1.0, 0.0)).g +
+//         getTUV(in.uv + pixelStep * vec2(0.0, 1.0)).b -
+//         getTUV(in.uv + pixelStep * vec2(0.0, -1.0)).b
+//     );
 
-    var x = ij.x - dt0 * data.g;
-    var y = ij.y - dt0 * data.b;
+//     // Write divergence into alpha temporarily
+//     return vec4f(data.rgb, divergence);
+// }
 
-    var xy = ij - dt0 * data.gb;
+// @fragment
+// fn fluid_pressure(in: VertexOutput) -> @location(0) vec4f {
+//     var dimensions = vec2f(textureDimensions(fieldTexture));
+//     var pixelStep = vec2(1.0) / dimensions;
 
-    var ij0 = floor(xy);
-    var ij1 = ij0 + 1.0;
+//     const DIFF = 0.00008;
 
-    var st1 = xy - ij0;
-    var st0 = 1.0 - st1;
+//     var data = getTUV(in.uv);
 
-    var p00 = getTXY(ij0, dimensions).r;
-    var p01 = getTXY(vec2f(ij0.x, ij1.y), dimensions).r;
-    var p10 = getTXY(vec2f(ij1.x, ij0.y), dimensions).r;
-    var p11 = getTXY(ij1, dimensions).r;
+//     // Diffuse
+//     var dt = 1.0 / 144.0;
+//     var a = dt * DIFF * dimensions.x * dimensions.y;
+//     var density = data.r + a * (
+//         getTUV(in.uv + pixelStep * vec2(-1.0, 0.0)).r +
+//         getTUV(in.uv + pixelStep * vec2(1.0, 0.0)).r +
+//         getTUV(in.uv + pixelStep * vec2(0.0, 1.0)).r +
+//         getTUV(in.uv + pixelStep * vec2(0.0, -1.0)).r -
+//         4.0 * data.r
+//     );
 
-    var density = 
-        st0.x * (st0.y * p00 + st1.y * p01) +
-        st1.x * (st0.y * p10 + st1.y * p11);
+//     var color_out = vec4f(density, data.gba);
 
-    var color_out = vec4f(density, data.gba);
+//     return color_out;
+// }
 
-    return color_out;
-}
+
+// @fragment
+// fn fluid_subtract_gradient(in: VertexOutput) -> @location(0) vec4f {
+//     var dimensions = vec2f(textureDimensions(fieldTexture));
+//     var pixelStep = vec2(1.0) / dimensions;
+
+//     var data = getTUV(in.uv);
+
+//     var divergence = 0.5 * pixelStep.x * (
+//         getTUV(in.uv + pixelStep * vec2(1.0, 0.0)).g -
+//         getTUV(in.uv + pixelStep * vec2(-1.0, 0.0)).g +
+//         getTUV(in.uv + pixelStep * vec2(0.0, 1.0)).b -
+//         getTUV(in.uv + pixelStep * vec2(0.0, -1.0)).b
+//     );
+
+//     // Write divergence into alpha temporarily
+//     return vec4f(data.rgb, divergence);
+// }
